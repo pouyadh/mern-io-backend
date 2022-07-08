ff04e87dc7f5ff6c894daace0c131dadd0f477c6
pouya <pouyadh.dev@gmail.com>
2022-07-09 03:17:03 +0430

    fix typo

--- middleware/checkMembership.js
+++ middleware/checkMembership.js
@@ -2,8 +2,7 @@ const RESP = require('../response/RESP');
 function attachUserRole(req, res, next) {
     const { username } = req.user;
     const { conversation } = req;
-    const userAsMember = conversation.getMemberByUsername(username);
-    if (!userAsMember) return next(RESP.NOT_MEMBER);
+    if (!conversation.isMember(username)) return next(RESP.NOT_MEMBER);
     next();
 }
 
